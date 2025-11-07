a96a132d0e363a3ff816ad18755d9f33ceb397d8
debsaikia03 <143481057+debsaikia03@users.noreply.github.com>
2025-11-08 02:58:29 +0530

    update

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { toast } from 'sonner';
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL: "https://medi-bridge-backend-r95j.onrender.com/api/v1",
   headers: {
     'Content-Type': 'application/json',
   },
